a81507e4a826f7b44919cf98c81f57f1b314af41
swiedjaja <swiedjaja@gmail.com>
2022-01-13 00:23:09 +0700

    digital lux meter for Esp8266 and ESP32

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "BH1750.h"
+#include "device.h"
+
+BH1750 lightMeter;
+
+void setup() {
+  Serial.begin(115200);
+  delay(200);
+  Wire.begin(PIN_SDA, PIN_SCL, 100000);
+  lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE, 0x23, &Wire);
+  Serial.println("System ready...");
+}
+
+void loop() {
+  float lux = lightMeter.readLightLevel();
+  Serial.printf("Light: %.2f lx\n", lux);
+  delay(1000);
+}
