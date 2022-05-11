dbf430a6f54d5c187ccc9bc348e3df8d1ed65fc2
swiedjaja <swiedjaja@gmail.com>
2022-05-12 02:23:30 +0700

    add esp32-c3

--- include/device.h
+++ include/device.h
@@ -7,7 +7,7 @@
   #define LED_GREEN  D6
   #define LED_YELLOW D7
   #define LED_RED    D8
-#elif defined(ESP32)
+#elif defined(ESP32_CAM)
   #if !defined(LED_BUILTIN) // ESP32Cam
     #define LED_BUILTIN 33
   #endif
@@ -19,6 +19,21 @@
   #define PIN_DHT    13 // pin 2, 12: program failed, 16: dht read error
   #define PIN_SDA    14
   #define PIN_SCL    15
+#elif defined(NODEMCU_ESP32_C3_13)
+  #define LED_BUILTIN_RED GPIO_NUM_3
+  #define LED_BUILTIN_GREEN GPIO_NUM_4
+  #define LED_BUILTIN_BLUE GPIO_NUM_5
+  #define LED_BUILTIN_COOL GPIO_NUM_19
+  #define LED_BUILTIN_WARM GPIO_NUM_18
+  #define LED_BUILTIN LED_BUILTIN_WARM
+  #define LED_RED     LED_BUILTIN_RED
+  #define LED_YELLOW  LED_BUILTIN_WARM
+  #define LED_GREEN   LED_BUILTIN_GREEN
+  #define PIN_SW     6
+  #define PIN_SDA    1
+  #define PIN_SCL    2
+  // #define PIN_DHT    2 // pin 2, 12: program failed, 16: dht read error
+  
 #endif
 
 #define LED_BUILTIN_ON  0
--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,33 @@ BH1750 lightMeter;
 void setup() {
   Serial.begin(115200);
   delay(200);
+  #if defined(NODEMCU_ESP32_C3_13)  
+    pinMode(LED_BUILTIN_COOL, OUTPUT);
+  #endif  
+  pinMode(LED_BUILTIN, OUTPUT);
+  delay(1000);
+
+  Serial.printf("Arduino Board: %s variant: %s\n", ARDUINO_BOARD, ARDUINO_VARIANT);
+  Serial.printf("Chip model: %s, Core: %d, CPU Freq: %u\n", 
+    ESP.getChipModel(), ESP.getChipCores(), ESP.getCpuFreqMHz());
+  Serial.printf("Flash size: %u, speed: %u, mode: %d\n", ESP.getFlashChipSize(), ESP.getFlashChipSpeed(), ESP.getFlashChipMode());
+  Serial.printf("PSRAM size: %u, free: %u\n", ESP.getPsramSize(), ESP.getFreePsram());
+  Serial.printf("Heap size: %u, free: %u\n", ESP.getHeapSize(), ESP.getFreeHeap());
+
   Wire.begin(PIN_SDA, PIN_SCL);
   lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE, 0x23, &Wire);
   Serial.println("System ready...");
 }
 
+int nCount=0;
 void loop() {
   float lux = lightMeter.readLightLevel();
   Serial.printf("Light: %.2f lx\n", lux);
+  // delay(1000);
+  digitalWrite(LED_BUILTIN, LED_ON);
+  Serial.printf("Hello World %d\n", nCount++);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LED_OFF);
   delay(1000);
+
 }
